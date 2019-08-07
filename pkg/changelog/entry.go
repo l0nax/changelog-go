8888fdf9a08df6a2ce6243dde45d1fa8d66b15bf
Emanuel Bennici <benniciemanuel78@gmail.com>
2019-08-07 23:17:53 +0200

    Add changelog Package
    In the Package _changelog_ the Code to parse, create and remove of
    Changelog Entries (and the CHANGELOG.md File itself) resides in there.
    
    Signed-off-by: Emanuel Bennici <benniciemanuel78@gmail.com>

---
+++ pkg/changelog/entry.go
@@ -0,0 +1,10 @@
+package changelog
+
+import "gitlab.com/l0nax/changelog-go/pkg/entry"
+
+// AddEntry() creates a new Changelog Entry
+// by creating the Entry File under (default)
+// $ROOT/.changelogs/unrelease/$BRANCH_NAME-$RAND_STRING
+func AddEntry(entry entry.Entry) {
+
+}
