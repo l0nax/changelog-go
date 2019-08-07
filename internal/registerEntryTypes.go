a0b84b20f342e01c20b2c234114ee718a184f163
Emanuel Bennici <benniciemanuel78@gmail.com>
2019-08-07 21:55:08 +0200

    Register all other Entry Types
    Signed-off-by: Emanuel Bennici <benniciemanuel78@gmail.com>

--- internal/registerEntryTypes.go
+++ internal/registerEntryTypes.go
@@ -5,16 +5,25 @@ import (
 	"gitlab.com/l0nax/changelog-go/pkg/entry/entries"
 )
 
-// define base Pointer to Types
-var basePointer = []entry.ChangeEntry{
-	entries.Entry_Added{},
-}
+
+//// define base Pointer to Types
+//var basePointer []entry.ChangeEntry
 
 // RegisterAll() registers all (defined) Changelog Entry Types
 func RegisterAll() {
 	// --- Initialize base Pointer to Types --- //
-	added := entries.Entry_Added{}
+	var added entry.ChangeEntry = &entries.Entry_Added{}
+	var changed entry.ChangeEntry = &entries.Entry_Changed{}
+	var deprecated entry.ChangeEntry = &entries.Entry_Deprecated{}
+	var removed entry.ChangeEntry = &entries.Entry_Removed{}
+	var security entry.ChangeEntry = &entries.Entry_Security{}
+	var other entry.ChangeEntry = &entries.Entry_Other{}
 
 	// --- Register --- //
 	_ = EntryT.RegisterEntryType(&added)
+	_ = EntryT.RegisterEntryType(&changed)
+	_ = EntryT.RegisterEntryType(&deprecated)
+	_ = EntryT.RegisterEntryType(&removed)
+	_ = EntryT.RegisterEntryType(&security)
+	_ = EntryT.RegisterEntryType(&other)
 }
