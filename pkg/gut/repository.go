9c5ff2921933ede41a0dfe512a3d5be2c7f201af
Emanuel Bennici <benniciemanuel78@gmail.com>
2019-10-16 22:55:22 +0200

    Add new Package 'gut'
    Signed-off-by: Emanuel Bennici <benniciemanuel78@gmail.com>

---
+++ pkg/gut/repository.go
@@ -0,0 +1,30 @@
+// gut (git utils) implements some Helper Functions around the `git` Library to easier
+// interact with the Repository.
+package gut
+
+import (
+	"os"
+	"path"
+)
+
+// FindGitRoot returns the last found `.git` Directory from the base
+// Means if your are in $PATH/cmd/test and the Git Root is $PATH/
+// FuncGitRoot will return $PATH/
+// It will return an Error if the `.git/` Directory could not be found.
+func FindGitRoot(base string) (string, error) {
+	var _path = base
+
+	for {
+		if _path == "/" {
+			return "", os.ErrNotExist
+		}
+
+		// check if `$PATH/.git` exists, if not go one Directory back
+		if _, err := os.Stat(path.Join(_path, ".git/")); !os.IsNotExist(err) {
+			// found
+			return _path, nil
+		}
+
+		_path = path.Join(_path, "../")
+	}
+}
