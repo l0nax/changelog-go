07f97b0b00703d1c221da5f94e23c3b9c3ef43b5
Emanuel Bennici <eb@fabmation.de>
2019-10-18 19:05:38 +0200

    Add new Command

--- cmd/new.go
+++ cmd/new.go
@@ -22,36 +22,25 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
 // newCmd represents the new command
 var newCmd = &cobra.Command{
 	Use:   "new",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Create a new Changelog-Entry",
+	Long: `"new" creates a new Changelog-Entry so you can easily commit
+your entry.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("new called")
+		// get Author if enabled in Config
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(newCmd)
 
-	// Here you will define your flags and configuration settings.
+	newCmd.Flags().String("title", "", "Title of the Changelog Entry")
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// newCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// newCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	newCmd.MarkFlagRequired("title")
 }
