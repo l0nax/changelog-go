d22b356722521b7bb036a072f7cea017a6b56df7
Emanuel Bennici <eb@fabmation.de>
2019-11-13 15:56:44 +0100

    Implement CheckDir() Function into changelog Pkg
    CheckDir() will be used to check if all needed directories exists.

--- pkg/changelog/tools.go
+++ pkg/changelog/tools.go
@@ -2,8 +2,31 @@ package changelog
 
 // This File contains all basic Functions
 
+import (
+	"github.com/spf13/viper"
+	"os"
+	"path"
+)
+
 // CheckDir will check if the changelog directory exists
 // if not it will create and initialize it.
 func CheckDir() {
+	// get Changelog data directory
+	dataDir := viper.GetString("changelog.entryPath")
+	if len(dataDir) == 0 {
+		panic("'changelog.entryPath' must be specified!")
+	}
 
+	// create Directory structure if it does not exists
+	if _, err := os.Stat(dataDir); os.IsNotExist(err) {
+		err = os.Mkdir(path.Join(dataDir, "unreleased"), 0755)
+		if err != nil {
+			panic(err)
+		}
+
+		err = os.Mkdir(path.Join(dataDir, "released"), 0755)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
