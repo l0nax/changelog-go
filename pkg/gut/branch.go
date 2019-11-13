82067189760798b5caf982138ab8dc48c516877c
Emanuel Bennici <eb@fabmation.de>
2019-11-13 19:07:33 +0100

    Add 'GetCurrentBranchFromRepository' Function

---
+++ pkg/gut/branch.go
@@ -0,0 +1,35 @@
+package gut
+
+import (
+	"gopkg.in/src-d/go-git.v4/plumbing"
+
+	git "gopkg.in/src-d/go-git.v4"
+)
+
+func GetCurrentBranchFromRepository(repository *git.Repository) (string, error) {
+	branchRefs, err := repository.Branches()
+	if err != nil {
+		return "", err
+	}
+
+	headRef, err := repository.Head()
+	if err != nil {
+		return "", err
+	}
+
+	var currentBranchName string
+	err = branchRefs.ForEach(func(branchRef *plumbing.Reference) error {
+		if branchRef.Hash() == headRef.Hash() {
+			currentBranchName = branchRef.Name().String()
+
+			return nil
+		}
+
+		return nil
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return currentBranchName, nil
+}
