0e2239875061eb1caa36db296f331338ea843414
Emanuel Bennici <eb@fabmation.de>
2019-11-13 17:10:39 +0100

    Add GetAuthorName() to gut Package

---
+++ pkg/gut/author.go
@@ -0,0 +1,17 @@
+package gut
+
+import (
+	log "github.com/sirupsen/logrus"
+	gitcfg "github.com/tcnksm/go-gitconfig"
+)
+
+// GetAuthorName returns the author name configured in the git config
+func GetAuthorName() (string, error) {
+	name, err := gitcfg.Username()
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+
+	return name, nil
+}
