14bc067a36d61a7fbcb3dd47b2815e46678baf66
Emanuel Bennici <eb@fabmation.de>
2019-11-13 19:07:54 +0100

    Implement Function to generate Random String

---
+++ pkg/tools/rand.go
@@ -0,0 +1,24 @@
+// Package tools implements some Functions which are needed to run this Application
+import tools
+
+import (
+	"crypto/rand"
+	log "github.com/sirupsen/logrus"
+)
+
+const randStrLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandomString(length int) string {
+	randData := make([]byte, length)
+	_, err := rand.Read(randData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i, rByte := range randData {
+		randData[i] = randStrLetters[randData[i] % byte(len(randStrLetters))]
+	}
+
+	return string(randData)
+}
+
