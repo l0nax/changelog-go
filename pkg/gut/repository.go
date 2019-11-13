2d53956dd1709a08de692d2c86f97a7f0c0d18c5
Emanuel Bennici <eb@fabmation.de>
2019-11-13 17:31:15 +0100

    Add constant maxDepth in gut Package

--- pkg/gut/repository.go
+++ pkg/gut/repository.go
@@ -7,6 +7,12 @@ import (
 	"path"
 )
 
+// maxDepth describes how many levels of directories this application will
+// search for the Changelog Config.
+// This is needed because otherwise the application will search for the Config
+// forever.
+const maxDepth = 16
+
 // FindGitRoot returns the last found `.git` Directory from the base
 // Means if your are in $PATH/cmd/test and the Git Root is $PATH/
 // FuncGitRoot will return $PATH/
