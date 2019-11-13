33faef76b69e08f7374659e944c6c88447446817
Emanuel Bennici <eb@fabmation.de>
2019-11-13 17:50:18 +0100

    Implement 'new' Command Functionality

--- cmd/new.go
+++ cmd/new.go
@@ -22,7 +22,18 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"bufio"
+	"fmt"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"gitlab.com/l0nax/changelog-go/internal"
+	"gitlab.com/l0nax/changelog-go/pkg/changelog"
+	"gitlab.com/l0nax/changelog-go/pkg/entry"
+	"gitlab.com/l0nax/changelog-go/pkg/gut"
+	"os"
+	"strconv"
+	"strings"
 )
 
 // newCmd represents the new command
@@ -34,10 +45,66 @@ your entry.`,
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		// check if first Argument is set
-		// title := args[0]
+		title := args[0]
+
+		if len(title) == 0 {
+			log.Fatalln("The Title of your Change MUST be specified!")
+		}
+
+		// check if needed Directories exists
+		changelog.CheckDir()
+
+		var author string
+		var err error
 
 		// get Author if enabled in Config
+		if viper.GetBool("entry.author") {
+			log.Debug("'entry.author' is enabled, so the Author will be grabbed and used.")
 
+			// get the Author
+			author, err = gut.GetAuthorName()
+			if err != nil {
+				os.Exit(1)
+			}
+		}
+
+		// create Entry
+		changelogEntry := entry.Entry{
+			ChangeTitle: title,
+			Author:      author,
+		}
+
+		// get all available Entry Types
+		for i, entryType := range internal.EntryT.ListAvailableTypes() {
+			fmt.Printf("[%d] %s\n", i, (*entryType).GetTypeDescription())
+		}
+
+		reader := bufio.NewReader(os.Stdin)
+
+		// ask User about Entry Type
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+
+		// convert input to Number
+		var choosenType int
+
+		if choosenType, err = strconv.Atoi(text); err != nil {
+			log.Fatalln("Please enter only VALID number!")
+		}
+
+		// check if Number does exists in our Entry Types List
+		if choosenType >= (len(internal.EntryT.ListAvailableTypes()) - 1) {
+			log.Fatalln("Please choose a VALID number!")
+		}
+
+		// create Entry
+		changelogEntry.Type = internal.EntryT.ListAvailableTypes()[choosenType]
+		changelog.AddEntry(changelogEntry)
 	},
 }
 
