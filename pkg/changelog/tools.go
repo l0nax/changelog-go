b4294f4c3621756272d9af4d7e674390d202637c
Emanuel Bennici <eb@fabmation.de>
2019-11-13 15:37:12 +0100

    Create skeleton tools.go in changelog Package

---
+++ pkg/changelog/tools.go
@@ -0,0 +1,9 @@
+package changelog
+
+// This File contains all basic Functions
+
+// CheckDir will check if the changelog directory exists
+// if not it will create and initialize it.
+func CheckDir() {
+
+}
