5f2e550ee52bef726a404619109344e6cb886a9a
Emanuel Bennici <eb@fabmation.de>
2019-12-19 18:42:46 +0100

    Remove unused imports in tools pkg

--- pkg/tools/entryStruct.go
+++ pkg/tools/entryStruct.go
@@ -3,9 +3,6 @@ package tools
 import (
 	// "fmt"
 
-	"github.com/kr/pretty"
-	log "github.com/sirupsen/logrus"
-
 	"gitlab.com/l0nax/changelog-go/internal"
 	"gitlab.com/l0nax/changelog-go/pkg/entry"
 	"gopkg.in/yaml.v2"
