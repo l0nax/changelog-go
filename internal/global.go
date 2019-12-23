f89069ff5a0338d89111074f0cfe6229b340b831
Emanuel Bennici <eb@fabmation.de>
2019-12-23 15:06:31 +0100

    Add more documentation to GitPath variable

--- internal/global.go
+++ internal/global.go
@@ -1,6 +1,9 @@
 package internal
 
 var (
-	GitPath string // Path to the top git Directory
+	// Path to the top git Directory. This variable does NOT contain the path
+	// to the `.git` directory, it contains the path to the Root directory
+	// of the project – which contains the `.git` directory.
+	GitPath string
 	Cwd     string // Path to the current working directory
 )
