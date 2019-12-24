5a8c3dd2175ba17fb64fe2ad772e0b7d83f4ff66
Emanuel Bennici <eb@fabmation.de>
2019-12-24 11:41:48 +0100

    Implement moveToReleaseFolder()

--- pkg/changelog/file.go
+++ pkg/changelog/file.go
@@ -1,9 +1,12 @@
 package changelog
 
 import (
-	"errors"
 	"os"
 	"path"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/spf13/viper"
 	"gitlab.com/l0nax/changelog-go/internal"
@@ -12,17 +15,46 @@ import (
 // createReleaseDir creates the needed directory structure for all Changelog
 // releases
 func createReleaseDir(version string) error {
-
 	basePath := path.Join(internal.GitPath, viper.GetString("changelog.entryPath"))
-	releasedPath := path.Join(basePath, "released")
+	releasedPath := path.Join(basePath, "released", version)
 	// unreleased := path.Join(basePath, "unreleased")
 
-	verPath := path.Join(releasedPath, version)
+	log.Debugf("New release path: '%s'\n", releasedPath)
 
 	// NOTE: Should we add an 'force' flag to delete the existing directory?
-	if _, err := os.Stat(verPath); !os.IsNotExist(err) {
-		// error since this path already exists
+	if _, err := os.Stat(releasedPath); !os.IsNotExist(err) {
+		// error because this release already exists
 		return errors.New("Release Path already exists.")
+	}
+
+	err := os.MkdirAll(releasedPath, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// moveToReleaseFolder moves all unreleased changelog-entry files to the specifc
+// release directory.
+func moveToReleaseFolder(version string) error {
+	basePath := path.Join(internal.GitPath, viper.GetString("changelog.entryPath"))
+	releasedPath := path.Join(basePath, "released", version)
+	unreleased := path.Join(basePath, "unreleased")
+
+	// get all files from the unrealed folder
+	err := filepath.Walk(unreleased, func(_path string, info os.FileInfo, err error) error {
+		// check if type is file, if not just skip
+		if info.IsDir() {
+			return nil
+		}
+
+		// since it's a file, we can now move it to the new directory.
+		return os.Rename(_path, path.Join(releasedPath, info.Name()))
+	})
+
+	if err != nil {
+		return errors.Wrap(err, "Error while moving changelog-entry to release directory")
 	}
 
 	return nil
